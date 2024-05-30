5d1331fa3f044966081c198e21cf086c68d8cbf4
casulit <chrisgelosulit@gmail.com>
2024-05-30 10:14:52 +0800

    refactor: Use env module for Convex URL config

--- components/convex/ConvexClientProvider.tsx
+++ components/convex/ConvexClientProvider.tsx
@@ -1,10 +1,11 @@
 'use client';
+import { env } from '@/env';
 import { useAuth as useClerkAuth } from '@clerk/nextjs';
 import { ConvexReactClient } from 'convex/react';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ReactNode } from 'react';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL);
 
 export default function ConvexClientProvider({
   children,
